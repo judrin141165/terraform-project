e211240feca3cac71367bb7d0cb7f6eb02455d6e
judrin1411165 <Red5t0ne>
2022-02-28 22:28:07 +0000

    initial commit

---
+++ backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = ">=1.0"
+  backend "s3" {
+  region = "us-east-1"
+  profile = "default"
+
+  key = "terraformstatefile"
+  bucket = "judrin-terraform-bucket"
+
+  }
+}
