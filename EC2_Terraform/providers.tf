ec9cdfedccf5ba04e6e8e44e3827299316fddd45
judrin1411165 <Red5t0ne>
2023-10-21 17:39:08 +0100

    Git update

---
+++ EC2_Terraform/providers.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  profile    = "var.profile"
+  region     = "us-east-2"
+  alias      = "region-master"
+  access_key = "AKIAYMIH6UZFVY6NANPL"
+  secret_key = "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"
+}
+
+provider "aws" {
+profile = "var.profile"
+region = "us-east-1"
+alias = "region-worker"
+access_key = "AKIAYMIH6UZFVY6NANPL"
+secret_key = "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"
+
+}
